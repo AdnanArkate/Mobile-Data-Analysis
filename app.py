8b09bf40e118b6f47e777386969a9dd8aa3c8c43
AdnanArkate <adnanarkate@gmail.com>
2026-03-16 18:24:39 +0530

    app.py

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-
+# Title
 st.title(" Mobile Phone Data Analysis")
 
-
+# Load Dataset
 df = pd.read_excel("Mobile_Phone_Data.xlsx")
 st.subheader("Dataset Preview")
 st.dataframe(df.head())
 
-# Cleaning 
-df = df.drop_duplicates()           
+# -------------------- DATA CLEANING --------------------
+df = df.drop_duplicates()
 df.columns = df.columns.str.strip().str.lower()
 df['price'] = df['price'].astype(int)
 
-# metrics
+# -------------------- DATA ANALYSIS --------------------
 st.subheader("Data Analysis Results")
 st.text(f"Average Price: {df['price'].mean():.2f}")
 st.text(f"Max Rating: {df['rating'].max()}")
@@ -26,22 +26,22 @@ st.dataframe(df.groupby('brand')['price'].mean())
 st.write("Brand Counts:")
 st.dataframe(df['brand'].value_counts())
 
-
+# -------------------- DATA VISUALIZATION --------------------
 st.subheader("Charts")
 
-# brand count (bar) 
+# Bar Chart
 st.write(" Number of Phones by Brand")
 fig1, ax1 = plt.subplots()
 df['brand'].value_counts().plot(kind='bar', color='skyblue', ax=ax1)
 st.pyplot(fig1)
 
-# rating (pie) 
+# Pie Chart
 st.write(" Ratings Distribution")
 fig2, ax2 = plt.subplots()
 df['rating'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax2)
 st.pyplot(fig2)
 
-# Price vs battery (scatter)
+# Scatter Plot
 st.write(" Price vs Battery Capacity")
 fig3, ax3 = plt.subplots()
 sns.scatterplot(x='price', y='battery(mah)', data=df, hue='brand', ax=ax3)
